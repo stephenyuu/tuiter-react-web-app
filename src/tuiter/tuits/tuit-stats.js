1991d90ad837004994f30bbf56af70d3bb6ad07d
Stephen Yu <stephenyu1229@gmail.com>
2023-03-21 11:30:21 -0400

    change like button implementation

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { updateTuitStats } from "./tuits-reducer";
 
 const TuitStats = ({
   tuit = {
@@ -10,12 +8,6 @@ const TuitStats = ({
     likes: 0,
   },
 }) => {
-  const dispatch = useDispatch();
-  const handleUnlikeTuit = () => {
-    dispatch(
-      updateTuitStats({ ...tuit, liked: !tuit.liked, likes: tuit.likes - 1 })
-    );
-  };
   return (
     <ul className="d-flex justify-content-around align-items-center">
       <li>
@@ -25,14 +17,12 @@ const TuitStats = ({
         <i className="bi bi-arrow-clockwise"></i> {tuit.retuits}
       </li>
       <li>
-        {tuit.liked ? (
-          <i
-            className="bi bi-heart-fill text-danger"
-            onClick={handleUnlikeTuit}
-          ></i>
-        ) : (
-          <i className="bi bi-heart"></i>
-        )}{" "}
+        <i
+          onClick={handle}
+          className={`bi ${
+            tuit.liked ? "bi-heart-fill text-danger" : "bi-heart"
+          }`}
+        ></i>{" "}
         {tuit.likes}
       </li>
       <li>
